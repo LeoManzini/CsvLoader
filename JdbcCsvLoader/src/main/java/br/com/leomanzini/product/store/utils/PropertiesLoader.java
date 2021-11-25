c4ec55f6c753a567711d4e3c12257b8942155882
Leonardo Henrique Manzini <leoh.manzini@gmail.com>
2021-11-24 21:59:32 -0300

    Adding some identations corrections

--- JdbcCsvLoader/src/main/java/br/com/leomanzini/product/store/utils/PropertiesLoader.java
+++ JdbcCsvLoader/src/main/java/br/com/leomanzini/product/store/utils/PropertiesLoader.java
@@ -6,17 +6,17 @@ import java.util.Properties;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-public abstract class PropertiesLoader{
+public abstract class PropertiesLoader {
 	
 	private static final Logger log = LogManager.getLogger(PropertiesLoader.class);
-
+	
 	private static final String PROPERTIES_DB_URL = "database.url";
 	private static final String PROPERTIES_DB_USER = "database.user";
 	private static final String PROPERTIES_DB_PASSWORD = "database.password";
 	
 	private static final String PROPERTIES_FILE_DESTINATION = "file.destination";
 	private static final String PROPERTIES_FILE_NAME = "file.name";
-
+	
 	private static String databaseUrl;
 	private static String databaseUser;
 	private static String databasePassword;
@@ -26,7 +26,7 @@ public abstract class PropertiesLoader{
 	
 	private PropertiesLoader() {
 	}
-
+	
 	public static void load(String path) {
 		
 		Properties properties = new Properties();
