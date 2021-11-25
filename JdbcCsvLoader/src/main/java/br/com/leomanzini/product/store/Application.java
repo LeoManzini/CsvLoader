4080073b415b23d950b371e0b2ee57e9142b08de
Leonardo Henrique Manzini <leoh.manzini@gmail.com>
2021-11-24 23:45:53 -0300

    JDBC error version

--- JdbcCsvLoader/src/main/java/br/com/leomanzini/product/store/Application.java
+++ JdbcCsvLoader/src/main/java/br/com/leomanzini/product/store/Application.java
@@ -1,9 +1,12 @@
 package br.com.leomanzini.product.store;
 
+import java.sql.Connection;
 import java.sql.SQLException;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
+import br.com.leomanzini.product.store.connector.PostgresConnector;
 
 public class Application {
 
@@ -11,6 +14,14 @@ public class Application {
 	
 	public static void main(String[] args) throws SQLException {
 		
-		log.info("Hello World!!");
+		try(PostgresConnector postgres = new PostgresConnector(); 
+				Connection connection = postgres.startDatabaseConnection(args[0])) {
+			log.info("Conectou na base suave");
+		} catch (Exception e) {
+			log.error("Não conectou na base");
+			System.exit(-2);
+		}
+		
+		log.info("Final feliz");
 	}
 }
