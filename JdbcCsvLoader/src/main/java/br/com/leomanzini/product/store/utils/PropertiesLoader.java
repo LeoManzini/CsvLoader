16227cd2264fba9ddd131ee59a1c113ccb71503c
Leonardo Henrique Manzini <leoh.manzini@gmail.com>
2021-11-24 22:02:39 -0300

    Deleting some lines unused at the PropertiesLoader class

--- JdbcCsvLoader/src/main/java/br/com/leomanzini/product/store/utils/PropertiesLoader.java
+++ JdbcCsvLoader/src/main/java/br/com/leomanzini/product/store/utils/PropertiesLoader.java
@@ -14,15 +14,9 @@ public abstract class PropertiesLoader {
 	private static final String PROPERTIES_DB_USER = "database.user";
 	private static final String PROPERTIES_DB_PASSWORD = "database.password";
 	
-	private static final String PROPERTIES_FILE_DESTINATION = "file.destination";
-	private static final String PROPERTIES_FILE_NAME = "file.name";
-	
 	private static String databaseUrl;
 	private static String databaseUser;
 	private static String databasePassword;
-	
-	private static String fileDestination;
-	private static String fileName;
 	
 	private PropertiesLoader() {
 	}
@@ -30,40 +24,29 @@ public abstract class PropertiesLoader {
 	public static void load(String path) {
 		
 		Properties properties = new Properties();
-
+		
 		try {
 			properties.load(new FileInputStream(path));
-
+			
 			databaseUrl = properties.getProperty(PROPERTIES_DB_URL);
 			databaseUser = properties.getProperty(PROPERTIES_DB_USER);
 			databasePassword = properties.getProperty(PROPERTIES_DB_PASSWORD);
-					
-			fileDestination = properties.getProperty(PROPERTIES_FILE_DESTINATION);
-			fileName = properties.getProperty(PROPERTIES_FILE_NAME);
 			
 		} catch (Exception e) {
 			log.error(e.getMessage(), e);
 			System.exit(-1);
 		}
 	}
-
+	
 	public static String getDatabaseUrl() {
 		return databaseUrl;
 	}
-
+	
 	public static String getDatabaseUser() {
 		return databaseUser;
 	}
-
+	
 	public static String getDatabasePassword() {
 		return databasePassword;
-	}
-
-	public static String getFileDestination() {
-		return fileDestination;
-	}
-	
-	public static String getFileName() {
-		return fileName;
 	}
 }
