c1c9d7ef4e4f85a8a30d4dcae21892f95fa6d72d
Leonardo Henrique Manzini <leoh.manzini@gmail.com>
2021-11-24 22:41:24 -0300

    Connection class

--- JdbcCsvLoader/src/main/java/br/com/leomanzini/product/store/Application.java
+++ JdbcCsvLoader/src/main/java/br/com/leomanzini/product/store/Application.java
@@ -1,5 +1,7 @@
 package br.com.leomanzini.product.store;
 
+import java.sql.SQLException;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -7,7 +9,7 @@ public class Application {
 
 	private static final Logger log = LogManager.getLogger(Application.class); 
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws SQLException {
 		
 		log.info("Hello World!!");
 	}
---
+++ JdbcCsvLoader/src/main/java/br/com/leomanzini/product/store/connector/PostgresConnector.java
@@ -0,0 +1,49 @@
+package br.com.leomanzini.product.store.connector;
+
+import java.io.Closeable;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import br.com.leomanzini.product.store.utils.PropertiesLoader;
+
+public class PostgresConnector implements DatabaseConnector, Closeable {
+	
+	private static final Logger log = LogManager.getLogger(PostgresConnector.class); 
+	
+	private Connection databaseConnection;
+
+	@Override
+	public Connection startDatabaseConnection(String propertiesPath) {
+		
+		PropertiesLoader.load(propertiesPath);
+		
+		if(databaseConnection == null) {
+			try {
+				databaseConnection = DriverManager.getConnection(
+						PropertiesLoader.getDatabaseUrl(), 
+						PropertiesLoader.getDatabaseUser(),
+						PropertiesLoader.getDatabasePassword());
+			} 
+			catch (SQLException e) {
+				log.error(e.getMessage(), e);
+				System.exit(-1);
+			}
+		}
+		
+		return databaseConnection;
+	}
+
+	@Override
+	public void close() {
+		try {
+			databaseConnection.close();
+		} catch (SQLException e) {
+			log.error(e.getMessage(), e);
+			System.exit(-1);
+		}
+	}
+}
