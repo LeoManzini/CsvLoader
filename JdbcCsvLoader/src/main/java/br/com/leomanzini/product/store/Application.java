03a123dcf6c554f25c77ed1aa0e934c5715b3b0f
Leonardo Henrique Manzini <leoh.manzini@gmail.com>
2021-11-27 10:46:24 -0300

    Using the executor class

--- JdbcCsvLoader/src/main/java/br/com/leomanzini/product/store/Application.java
+++ JdbcCsvLoader/src/main/java/br/com/leomanzini/product/store/Application.java
@@ -5,22 +5,34 @@ import org.apache.logging.log4j.Logger;
 
 import br.com.leomanzini.product.store.enums.ErrorMessages;
 import br.com.leomanzini.product.store.exceptions.ParametersException;
+import br.com.leomanzini.product.store.executor.Executor;
+import br.com.leomanzini.product.store.executor.PersistenceExecutor;
 
 public class Application {
 
-	private static final Logger log = LogManager.getLogger(Application.class); 
-	
+	private static final Logger log = LogManager.getLogger(Application.class);
+
 	public static void main(String[] args) throws Exception {
-		
-		if(args.length != 2) {
+
+		if (args.length != 2) {
 			log.error("Invalid arguments number!");
-			
+
 			log.info("Expected: ");
 			log.info("java -jar CsvLoader <propertiesPath> <csvPath>");
 			log.info("<propertiesPath> where the properties of your project are;");
 			log.info("<csvPath> where the project will find your csv file to load.");
-			
+
 			throw new ParametersException(ErrorMessages.PARAMETER_ERROR);
+		}
+
+		String propertiesPath = args[0];
+		String csvPath = args[1];
+
+		try {
+			Executor persistenceExecutor = new PersistenceExecutor();
+			persistenceExecutor.execute(propertiesPath, csvPath);
+		} catch (Exception e) {
+
 		}
 	}
 }
---
+++ JdbcCsvLoader/src/main/java/br/com/leomanzini/product/store/executor/PersistenceExecutor.java
@@ -0,0 +1,10 @@
+package br.com.leomanzini.product.store.executor;
+
+public class PersistenceExecutor implements Executor {
+
+	@Override
+	public void execute(String propertiesPath, String csvPath) {
+		
+		
+	}
+}
