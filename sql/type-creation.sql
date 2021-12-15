7c7c1c94b25920038c0f39d6ce95a87e01647984
Leonardo Henrique Manzini <leoh.manzini@gmail.com>
2021-12-14 23:13:06 -0300

    Version with errors at create a type process

--- sql/type-creation.sql
+++ sql/type-creation.sql
@@ -1,9 +1,24 @@
-CREATE TYPE products AS (
+CREATE TYPE stores AS (
 	id INTEGER,
-	nome VARCHAR(255)
+	nome VARCHAR(255),
+	documento VARCHAR(255)
 );
 
-CREATE OR REPLACE FUNCTION products_test(producto IN products[])
+CREATE TYPE inventories AS (
+    id INTEGER,
+    product_id INTEGER,
+    store_id INTEGER,
+    amount INTEGER,
+    price NUMERIC(15,2)
+);
+
+CREATE TYPE products AS (
+	id INTEGER,
+	nome VARCHAR(255),
+	inventory INVENTORIES
+);
+
+CREATE OR REPLACE FUNCTION products_test(store IN stores, producto IN products[])
 RETURNS products[] AS $$
 BEGIN
 	RETURN producto;
@@ -11,9 +26,20 @@ END;
 $$ LANGUAGE plpgsql;
 
 SELECT products_test(ARRAY[
-	'(1,"tv")',
-	'(2,"PC")'
+	'(1,"tv", {1, 1, 1, 20, 20.00}::inventories)',
+	'(2,"PC", INVENTORIES(1, 1, 1, 20, 20.00))'
 ]::products[]);
+
+SELECT ARRAY[
+	'(1,"tv", inventories(1, 1, 1, 20, 20.00))',
+	'(2,"PC", INVENTORIES(1, 1, 1, 20, 20.00))'
+]::products[];
+
+SELECT c.product_id, c.store_id, c.price FROM inventories c;
+
+SELECT p FROM products p;
+
+SELECT s FROM stores s;
 
 DROP FUNCTION products_test(products[]);
 DROP TYPE products CASCADE;
