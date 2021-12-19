f9fd493052d8187026bad441ecdc18c785b82f5d
Leonardo Henrique Manzini <leoh.manzini@gmail.com>
2021-12-19 16:15:47 -0300

    Errors list updated

--- JdbcCsvLoader/src/main/java/br/com/leomanzini/product/store/enums/ErrorMessages.java
+++ JdbcCsvLoader/src/main/java/br/com/leomanzini/product/store/enums/ErrorMessages.java
@@ -3,18 +3,31 @@ package br.com.leomanzini.product.store.enums;
 public enum ErrorMessages {
 	
 	PARAMETER_ERROR("Invalid arguments number, exit -1"),
+	
 	PROPERTIES_LOADER_ERROR("Something went wrong while reading properties file, exit -2"),
+	
 	CSV_READER_ERROR("Something went wrong while reading csv file, exit -3"),
 	CSV_EXECUTOR_ERROR("Something wrong with your csv, exit -4"),
-	INSERT_STORE_DAO_ERROR("Could not persist the store entity, exit -5"),
-	SQL_INSERT_STORE_DAO_ERROR("Something went wrong with your SQL, exit -6"),
-	PERSISTENCE_EXECUTOR_ERROR("Something went wrong while executing the database operation, exit -7"),
 	
-	PRODUCT_INSERT_ERROR("Error while trying to insert data to product"),
-	PRODUCT_UPDATE_ERROR("Error while updating product"),
-	PRODUCT_DELETE_ERROR("Error while deleting product"),
-	PRODUCT_FIND_ERROR("Error while searching product"),
-	PRODUCT_FIND_ALL_ERROR("Error while searching for products");
+	STORE_INSERT_ERROR("Error while trying to insert data to store, exit -4"),
+	STORE_UPDATE_ERROR("Error while updating store, exit -5"),
+	STORE_DELETE_ERROR("Error while deleting store, exit -6"),
+	STORE_FIND_ERROR("Error while searching store, exit -7"),
+	STORE_FIND_ALL_ERROR("Error while searching for stores, exit -8"),
+	
+	PRODUCT_INSERT_ERROR("Error while trying to insert data to product, exit -9"),
+	PRODUCT_UPDATE_ERROR("Error while updating product, exit -10"),
+	PRODUCT_DELETE_ERROR("Error while deleting product, exit -11"),
+	PRODUCT_FIND_ERROR("Error while searching product, exit -12"),
+	PRODUCT_FIND_ALL_ERROR("Error while searching for products, exit -13"),
+	
+	INVENTORY_INSERT_ERROR("Error while trying to insert data to inventory, exit -14"),
+	INVENTORY_UPDATE_ERROR("Error while updating inventory, exit -15"),
+	INVENTORY_DELETE_ERROR("Error while deleting inventory, exit -16"),
+	INVENTORY_FIND_ERROR("Error while searching inventory, exit -17"),
+	INVENTORY_FIND_ALL_ERROR("Error while searching for inventories, exit -18"),
+	
+	PERSISTENCE_EXECUTOR_ERROR("Something went wrong while executing the database operation, exit -19");
 	
 	private String message;
 	
