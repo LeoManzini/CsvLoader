d9637f77f484de292544faca6172b5dc4a617786
Leonardo Henrique Manzini <leoh.manzini@gmail.com>
2022-01-21 16:30:57 -0300

    Persistence unity working on local tomcat

--- JpaRestCsvLoader/src/main/java/br/com/leomanzini/products/store/resources/HelloWorldResource.java
+++ JpaRestCsvLoader/src/main/java/br/com/leomanzini/products/store/resources/HelloWorldResource.java
@@ -1,6 +1,5 @@
 package br.com.leomanzini.products.store.resources;
 
-import br.com.leomanzini.products.store.utils.PropertiesLoader;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
@@ -12,7 +11,6 @@ public class HelloWorldResource {
 	@GET
 	@Produces(MediaType.TEXT_PLAIN)
 	public String helloWorld() {
-		PropertiesLoader.load();
-		return "Hello World! " + PropertiesLoader.getDatabaseUrl();
+		return "Hello World! ";
 	}
 }
