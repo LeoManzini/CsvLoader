5a5467f776a24de9e894c32c0aff8db9050a8f19
Leonardo Henrique Manzini <leoh.manzini@gmail.com>
2022-01-27 22:05:12 -0300

    Startimg work on service class

---
+++ JpaRestCsvLoader/src/main/java/br/com/leomanzini/products/store/services/StoreService.java
@@ -0,0 +1,66 @@
+package br.com.leomanzini.products.store.services;
+
+import org.jvnet.hk2.annotations.Service;
+
+import br.com.leomanzini.products.store.dao.GenericDaoInterface;
+import br.com.leomanzini.products.store.dao.impl.InventoryDaoImpl;
+import br.com.leomanzini.products.store.dao.impl.ProductDaoImpl;
+import br.com.leomanzini.products.store.dao.impl.StoreDaoImpl;
+import br.com.leomanzini.products.store.dto.ProductDto;
+import br.com.leomanzini.products.store.dto.ResponseObjectDto;
+import br.com.leomanzini.products.store.dto.StoreDto;
+import br.com.leomanzini.products.store.model.entities.Inventory;
+import br.com.leomanzini.products.store.model.entities.Product;
+import br.com.leomanzini.products.store.model.entities.Store;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+
+@Service
+public class StoreService {
+
+	private final GenericDaoInterface<Store> storeDao;
+	private final GenericDaoInterface<Product> productDao;
+	private final GenericDaoInterface<Inventory> inventoryDao;
+
+	public StoreService() {
+		storeDao = new StoreDaoImpl();
+		productDao = new ProductDaoImpl();
+		inventoryDao = new InventoryDaoImpl();
+	}
+
+	public StoreDto getStoreProducts() {
+		return null;
+	}
+
+	public ProductDto getStoreProduct(Integer id) {
+		return null;
+	}
+
+	public Response insertStore(StoreDto storeToInsert) {
+		return Response.ok(ResponseObjectDto.builder().build().toString(), MediaType.APPLICATION_JSON).build();
+	}
+
+	public Response insertProductIntoStore(ProductDto productToInsert) {
+		return Response.ok(ResponseObjectDto.builder().build().toString(), MediaType.APPLICATION_JSON).build();
+	}
+
+	public Response insertNewProductToDatabase(ProductDto productToInsert) {
+		return Response.ok(ResponseObjectDto.builder().build().toString(), MediaType.APPLICATION_JSON).build();
+	}
+
+	public Response updateStore(StoreDto storeToUpdate) {
+		return Response.ok(ResponseObjectDto.builder().build().toString(), MediaType.APPLICATION_JSON).build();
+	}
+
+	public Response updateProduct(ProductDto productToUpdate) {
+		return Response.ok(ResponseObjectDto.builder().build().toString(), MediaType.APPLICATION_JSON).build();
+	}
+	
+	public Response deleteStore(Integer id) {
+		return Response.ok(ResponseObjectDto.builder().build().toString(), MediaType.APPLICATION_JSON).build();
+	}
+
+	public Response deleteProduct(Long serial) {
+		return Response.ok(ResponseObjectDto.builder().build().toString(), MediaType.APPLICATION_JSON).build();
+	}
+}
