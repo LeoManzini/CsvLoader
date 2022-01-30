a76d1cc18d10166dfb504e2610f97cc2c2ccbbdb
Leonardo Henrique Manzini <leoh.manzini@gmail.com>
2022-01-30 10:06:36 -0300

    Store resources created

--- JpaRestCsvLoader/src/main/java/br/com/leomanzini/products/store/resources/StoreResource.java
+++ JpaRestCsvLoader/src/main/java/br/com/leomanzini/products/store/resources/StoreResource.java
@@ -1,20 +1,22 @@
 package br.com.leomanzini.products.store.resources;
 
 import br.com.leomanzini.products.store.services.StoreService;
+import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 
-@Path("/hello")
-public class HelloWorldResource {
+@Path("/store")
+public class StoreResource {
 	
 	StoreService service = new StoreService();
 
 	@GET
+	@Path("{document}")
 	@Produces(MediaType.APPLICATION_JSON)
-	public Response helloWorld() {
-		return service.getStoreProducts(10);
+	public Response getStoreProducts(@PathParam("document") Integer document) {
+		return service.getStoreProducts(document);
 	}
 }
