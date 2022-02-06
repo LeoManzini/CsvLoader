9ee8404d319d4c7be0ee930ca533c8f53d641d01
Leonardo Henrique Manzini <leoh.manzini@gmail.com>
2022-02-06 13:11:30 -0300

    Starting work on database insertion routine

--- JpaRestCsvLoader/src/main/java/br/com/leomanzini/products/store/services/FileUploadService.java
+++ JpaRestCsvLoader/src/main/java/br/com/leomanzini/products/store/services/FileUploadService.java
@@ -2,18 +2,23 @@ package br.com.leomanzini.products.store.services;
 
 import java.io.InputStream;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import br.com.leomanzini.products.store.csv.CsvReader;
 import br.com.leomanzini.products.store.dao.DaoFactory;
 import br.com.leomanzini.products.store.dao.impl.InventoryDaoImpl;
 import br.com.leomanzini.products.store.dao.impl.ProductDaoImpl;
 import br.com.leomanzini.products.store.dao.impl.StoreDaoImpl;
 import br.com.leomanzini.products.store.dto.StoreDto;
+import br.com.leomanzini.products.store.entities.Inventory;
 import br.com.leomanzini.products.store.exceptions.CsvReaderException;
 
 @SuppressWarnings("unused")
 public class FileUploadService {
 
 	private static FileUploadService fileUploadServiceImplementation;
+	private static final Logger log = LogManager.getLogger(FileUploadService.class);
 
 	private final StoreDaoImpl storeDao = DaoFactory.getStoreDaoImplementation();
 	private final ProductDaoImpl productDao = DaoFactory.getProductDaoImplementation();
@@ -29,12 +34,27 @@ public class FileUploadService {
 	public boolean executeDatabaseRoutine(InputStream inputStream) {
 		try {
 			CsvReader reader = new CsvReader();
-			StoreDto storeTopersist = reader.readCsv(inputStream);
-			System.out.println(storeTopersist);
+			StoreDto storeToPersist = reader.readCsv(inputStream);
+
+			if (storeDao.findById(storeToPersist.getStoreDocument()) != null) {
+				log.info("Store found at database, checking the products list for " + storeToPersist.getStoreName());
+				storeToPersist.getProducts().forEach(product -> {
+					try {
+						if (inventoryDao.findStoreProduct(storeToPersist.getStoreDocument(), product.getProductSerial()) != null) {
+							log.info("Product " + product.getProductName() + " found at this store, increasing inventory");
+							inventoryDao.update(Inventory.builder().build());
+							log.info("Inventory increased");
+						}
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+				});
+			}
+
 			return true;
 		} catch (CsvReaderException e) {
 			e.printStackTrace();
-			return false;	
+			return false;
 		}
 	}
 }
